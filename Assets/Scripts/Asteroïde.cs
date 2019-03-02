ed33832494d81a0dacbb6fabe1d0f72045c29f59
Florine <32348261+Florine34@users.noreply.github.com>
2019-03-01 22:51:00 -0500

    Add rotation asteroïde

--- Assets/Scripts/Asteroïde.cs
+++ Assets/Scripts/Asteroïde.cs
@@ -7,27 +7,44 @@ public class Asteroïde : MonoBehaviour
     private Rigidbody rbPlayer;
     private Rigidbody rbAsteroïde;
     public float speed;
+    private int randomRotation;
+    private Vector3 rotation; 
 
     // Start is called before the first frame update
     void Start()
     {
         rbAsteroïde = GetComponent<Rigidbody>();
+        randomRotation = Random.Range(1, 3);
+        Debug.Log(randomRotation); 
+        switch (randomRotation)
+        {
+                        case 1:
+                rotation = new Vector3(0.0f, 0.5f, 0.0f);
+              
+                break;
+            case 2:
+                rotation = new Vector3(0.0f, 0.0f, 0.5f);
+               
+                break; 
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.eulerAngles += rotation; 
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
-
             rbPlayer = other.GetComponent<Rigidbody>();
-            rbPlayer.AddForce(Vector3.down);
-            rbAsteroïde.AddForce(Vector3.down); 
+            rbAsteroïde.velocity = rbPlayer.velocity;
+            rbPlayer.velocity = rbPlayer.velocity * (-1.0f);
+           
         }
     }
 }
