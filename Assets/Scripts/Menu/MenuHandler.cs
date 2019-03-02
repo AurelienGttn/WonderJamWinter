dedd8a89d9f9e8af3dac18fd5e372a6850b3683f
deosadamas <deosadamaslol@gmail.com>
2019-03-02 16:06:29 -0500

    Menu insctruction in progress

--- Assets/Scripts/Menu/MenuHandler.cs
+++ Assets/Scripts/Menu/MenuHandler.cs
@@ -1,33 +1,67 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuHandler : MonoBehaviour
 {
     public GameObject canvasMenu;
     public GameObject canvasCredits;
+    public GameObject canvasIntruction;
+    public EventSystem eventSystem;
+    public GameObject startButton;
+    public GameObject backButton;
+
+  
+  
 
     public void Start()
     {
+        canvasMenu.SetActive(true);
+        canvasCredits.SetActive(false);
+
+
+       
+        ColorBlock colorBlock = new ColorBlock();
+        colorBlock.normalColor = new Color(255, 255, 255, 0);
+        colorBlock.highlightedColor = new Color(0, 0, 0, 101);
+
+
+        Debug.Log(colorBlock.normalColor);
+        Debug.Log(colorBlock.highlightedColor);
     }
 
     public void onClickbtn_Play()
     {
         SceneManager.LoadScene(1);
+       
+    }
+
+    public void onClickIntructions()
+    {
+        StartCoroutine("MenutoIntrcution");
+        eventSystem.SetSelectedGameObject(backButton);
     }
 
     public void onClickCredits()
     {
         StartCoroutine("menuToCredits");
+        eventSystem.SetSelectedGameObject(backButton);
     }
 
     public void onClickMenu()
     {
         StartCoroutine("creditsToMenu");
+        eventSystem.SetSelectedGameObject(startButton);
+
+    
+
     }
 
     public IEnumerator menuToCredits()
     {
+      
         float a = 1;
         while (a >= 0)
         {
@@ -36,7 +70,7 @@ public class MenuHandler : MonoBehaviour
             a -= 0.05f;
         }
         canvasMenu.SetActive(false);
-        canvasCredits.SetActive(true);
+        
 
         yield return new WaitForSeconds(0.5f);
         canvasCredits.SetActive(true);
@@ -48,10 +82,15 @@ public class MenuHandler : MonoBehaviour
             a += 0.05f;
         }
 
+      
+
+
+
     }
 
     public IEnumerator creditsToMenu()
     {
+     
         float a = 1;
         while (a >= 0)
         {
@@ -60,6 +99,8 @@ public class MenuHandler : MonoBehaviour
             a -= 0.05f;
         }
         canvasCredits.SetActive(false);
+
+
 
         yield return new WaitForSeconds(0.5f);
         canvasMenu.SetActive(true);
@@ -74,6 +115,59 @@ public class MenuHandler : MonoBehaviour
     }
 
 
+    public IEnumerator IntructionToMenu()
+    {
+
+        float a = 1;
+        while (a >= 0)
+        {
+            yield return new WaitForSeconds(0.01f);
+            setAlphaObject(canvasIntruction, a);
+            a -= 0.05f;
+        }
+        canvasCredits.SetActive(false);
+
+        yield return new WaitForSeconds(0.5f);
+        canvasMenu.SetActive(true);
+        setAlphaObject(canvasMenu, 0);
+        while (a <= 1)
+        {
+            yield return new WaitForSeconds(0.01f);
+            setAlphaObject(canvasMenu, a);
+            a += 0.05f;
+        }
+
+
+    }
+
+
+        public IEnumerator MenutoIntrcution()
+        {
+
+        float a = 1;
+        while (a >= 0)
+        {
+            yield return new WaitForSeconds(0.01f);
+            setAlphaObject(canvasMenu, a);
+            a -= 0.05f;
+        }
+        canvasMenu.SetActive(false);
+
+
+        yield return new WaitForSeconds(0.5f);
+        canvasIntruction.SetActive(true);
+        setAlphaObject(canvasIntruction, 0);
+        while (a <= 1)
+        {
+            yield return new WaitForSeconds(0.01f);
+            setAlphaObject(canvasIntruction, a);
+            a += 0.05f;
+        }
+
+
+
+    }
+
     public void setAlphaObject(GameObject myObject, float a)
     {
         foreach (Transform child in myObject.transform)
@@ -84,6 +178,8 @@ public class MenuHandler : MonoBehaviour
             }
             setAlphaObject(child.gameObject, a);
         }
+
+       
     }
 
     public void Exit() {
