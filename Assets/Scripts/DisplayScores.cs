ec133af0bd7ea9edced3e1b432c5ba9c00b7d823
AurelienGttn <aurelien.guitton@hotmail.com>
2019-03-02 11:55:16 -0500

    Add score

---
+++ Assets/Scripts/DisplayScores.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DisplayScores : MonoBehaviour
+{
+
+    private Score score;
+
+    private TextMeshProUGUI scoreText;
+
+
+    void Start()
+    {
+        score = FindObjectOfType<Score>();
+
+        scoreText = GetComponent<TextMeshProUGUI>();
+
+        scoreText.text = "Player 1 score : " + Mathf.FloorToInt(score.scoreP1)
+            + "\nPlayer 2 score : " + Mathf.FloorToInt(score.scoreP2);
+    }
+
+
+
+}
