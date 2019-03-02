7231d006645c02c87f476c3cb20f3ea0f90ad7a9
Florine <32348261+Florine34@users.noreply.github.com>
2019-03-01 21:41:34 -0500

    Ajout force opposé

---
+++ Assets/Scripts/Asteroïde.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroïde : MonoBehaviour
+{
+    private Rigidbody rbPlayer;
+    private Rigidbody rbAsteroïde;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbAsteroïde = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+
+            rbPlayer = other.GetComponent<Rigidbody>();
+            rbPlayer.AddForce(Vector3.down);
+            rbAsteroïde.AddForce(Vector3.down); 
+        }
+    }
+}
