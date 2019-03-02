1961f0634038108f408ffe436fe42b42c05a70b4
gduminy <guillaume.duminy@yahoo.fr>
2019-03-02 01:20:44 -0500

    push trigger de fin de niveau

---
+++ Assets/Scripts/TriggerFin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TriggerFin : MonoBehaviour
+{
+
+    public string NomLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Fin du jeu");
+            SceneManager.LoadScene(NomLevel, LoadSceneMode.Additive);
+        }
+    }
+}
