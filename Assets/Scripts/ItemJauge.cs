673f6d2fdcd321c1b7b6037e26d67a519cfa9496
Florine <32348261+Florine34@users.noreply.github.com>
2019-03-03 02:20:10 -0500

    Ajout item jauge

---
+++ Assets/Scripts/ItemJauge.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemJauge : MonoBehaviour, Items
+{
+    private GameObject player1;
+    private GameObject player2;
+    private float malus;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
+       
+    }
+
+    public void run(bool isPlayer1)
+    {
+        player1 = GameObject.Find("Player 1");
+        player2 = GameObject.Find("Player 2");
+        bool player1First = false; 
+        float positionPlayer1 = player1.transform.position.x;
+        float positionPlayer2 = player2.transform.position.x;
+        if (positionPlayer1 > positionPlayer2)
+        {
+            player1First = true; 
+        }
+
+        if (isPlayer1 && player1First)
+        {
+            malus = 10.0f;
+            player2.GetComponent<playerMovement>().pression -= malus;
+        }
+
+        else if (isPlayer1 && !player1First)
+        {
+            malus = 30.0f;
+            player2.GetComponent<playerMovement>().pression -= malus;
+        }
+
+        if(!isPlayer1 && !player1First)
+        {
+            malus = 10.0f;
+            player1.GetComponent<playerMovement>().pression -= malus;
+        }
+
+        if (!isPlayer1 && player1First)
+        {
+            malus = 30.0f;
+            player1.GetComponent<playerMovement>().pression -= malus;
+        }
+
+      
+
+    }
+}
---
+++ Assets/Scripts/RandomItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomItem : MonoBehaviour
+{
+    public List<GameObject> items;
+    private int randomIndex;
+    private bool player1; 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        player1 = other.GetComponent<playerMovement>().isPlayer1;
+        Debug.Log("Player : "+ player1); 
+        randomIndex = Random.Range(0, items.Count);
+        items[randomIndex].GetComponent<Items>().run(player1); 
+    }
+}
