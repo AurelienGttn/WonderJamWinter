62163e173f0969b1781fc83363fd612e42745c47
Griwald <griwald13@gmail.com>
2019-03-03 12:17:02 -0500

    Item declanchement

--- Assets/Scripts/RandomItem.cs
+++ Assets/Scripts/RandomItem.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RandomItem : MonoBehaviour
 {
@@ -15,14 +16,29 @@ public class RandomItem : MonoBehaviour
         {
             player1 = other.GetComponent<playerMovement>().isPlayer1;
             randomIndex = Random.Range(0, items.Count);
+
             if(player1)
             {
                 if(!playerPassage[0])
                 {
                     playerPassage[0] = true;
                     GameObject item = Instantiate(items[randomIndex]);
-                    item.GetComponent<Items>().run(player1);
+					other.gameObject.GetComponent<playerMovement>().item = item.GetComponent<Items>();
                    
+					switch (item.name) {
+						case "ItemJauge":
+							GameObject.Find("PressionJ1").GetComponent<Image>().enabled = true;
+							break;
+						case "ItemInvulnerabilite":
+							GameObject.Find("InvulnerabiliteJ1").GetComponent<Image>().enabled = true;
+							break;
+						case "ItemPushAsteroid":
+							GameObject.Find("RepoussoirJ1").GetComponent<Image>().enabled = true;
+							break;
+						case "ItemFlouJoueur":
+							GameObject.Find("FlouJ1").SetActive(true);
+							break;
+					}
                 }
             } else
             {
@@ -30,8 +46,24 @@ public class RandomItem : MonoBehaviour
                 {
                     playerPassage[1] = true;
                     GameObject item = Instantiate(items[randomIndex]);
-                    item.GetComponent<Items>().run(player1);
-                }
+                    //item.GetComponent<Items>().run(player1);
+					other.gameObject.GetComponent<playerMovement>().item = item.GetComponent<Items>();
+
+					switch (item.name) {
+						case "ItemJauge":
+							GameObject.Find("PressionJ2").SetActive(true);
+							break;
+						case "ItemInvulnerabilite":
+							GameObject.Find("InvulnerabiliteJ2").SetActive(true);
+							break;
+						case "ItemPushAsteroid":
+							GameObject.Find("RepoussoirJ2").SetActive(true);
+							break;
+						case "ItemFlouJoueur":
+							GameObject.Find("FlouJ2").SetActive(true);
+							break;
+					}
+				}
             }
         }
     }
