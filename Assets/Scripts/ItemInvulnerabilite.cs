2b1c33eb7d549507b4bd800323e1be46931faee9
Florine <32348261+Florine34@users.noreply.github.com>
2019-03-03 09:11:12 -0500

    Creation sphere invulnerabilite

--- Assets/Scripts/ItemInvulnerabilite.cs
+++ Assets/Scripts/ItemInvulnerabilite.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class ItemInvulnerabilite : MonoBehaviour, Items
 {
+
+    private GameObject player1;
+    private GameObject player2;
+    public GameObject invulnerabiliteJ1; 
+    public GameObject invulnerabiliteJ2;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +23,34 @@ public class ItemInvulnerabilite : MonoBehaviour, Items
 
     public void run(bool isPlayer1)
     {
+        player1 = GameObject.Find("Player 1");
+        player2 = GameObject.Find("Player 2");
+        bool player1First = false;
+        float positionPlayer1 = player1.transform.position.x;
+        float positionPlayer2 = player2.transform.position.x;
+        if (positionPlayer1 > positionPlayer2)
+        {
+            player1First = true;
+        }
 
+        if (isPlayer1 && player1First)
+        {
+            invulnerabiliteJ1.SetActive(true); 
+        }
+
+        if (isPlayer1 && !player1First)
+        {
+            invulnerabiliteJ1.SetActive(true);
+        }
+
+        if (!isPlayer1 && !player1First)
+        {
+            invulnerabiliteJ2.SetActive(true);
+        }
+
+        if (!isPlayer1 && player1First)
+        {
+            invulnerabiliteJ2.SetActive(true);
+        }
     }
 }
